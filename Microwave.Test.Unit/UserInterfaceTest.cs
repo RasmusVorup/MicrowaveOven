f5ea8aa33077022aef03bd2e042fb5c25ab4a27d
Samuel Lund Jessen <au646967@uni.au.dk>
2022-04-21 14:02:10 +0200

    Added test and changed test for userinterfaceclass

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -23,6 +23,7 @@ namespace Microwave.Test.Unit
         private ICookController cooker;
 
         private IBuzzer buzzer;
+        private int powertubeConfig = 500;
 
         [SetUp]
         public void Setup()
@@ -35,6 +36,7 @@ namespace Microwave.Test.Unit
             display = Substitute.For<IDisplay>();
             cooker = Substitute.For<ICookController>();
             buzzer = Substitute.For<IBuzzer>();
+            
 
             uut = new UserInterface(
                 powerButton, timeButton, startCancelButton,
@@ -42,7 +44,7 @@ namespace Microwave.Test.Unit
                 display,
                 light,
                 cooker, 
-                buzzer);
+                buzzer,powertubeConfig);
         }
 
         [Test]
@@ -85,19 +87,21 @@ namespace Microwave.Test.Unit
         }
 
         [Test]
-        public void Ready_14PowerButton_PowerIs700()
+        public void Ready_XPowerButtonPresses_PowerIsMax()
         {
-            for (int i = 1; i <= 14; i++)
+	        
+            for (int i = 1; i <= powertubeConfig/50; i++)
             {
                 powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
             }
-            display.Received(1).ShowPower(Arg.Is<int>(700));
+            display.Received(1).ShowPower(Arg.Is<int>(powertubeConfig));
         }
 
         [Test]
-        public void Ready_15PowerButton_PowerIs50Again()
+        public void PowerIs_50_AccordingToTimesPowerButtonPressed_AndMaxPowertubeConfig()
         {
-            for (int i = 1; i <= 15; i++)
+            
+            for (int i = 1; i <= (powertubeConfig/50)+1; i++)
             {
                 powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
             }
@@ -217,7 +221,7 @@ namespace Microwave.Test.Unit
         [Test]
         public void Ready_FullPower_CookerIsCalledCorrectly()
         {
-            for (int i = 50; i <= 700; i += 50)
+            for (int i = 50; i <= powertubeConfig; i += 50)
             {
                 powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
             }
@@ -228,7 +232,7 @@ namespace Microwave.Test.Unit
             // Should call with correct values
             startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
 
-            cooker.Received(1).StartCooking(700, 60);
+            cooker.Received(1).StartCooking(powertubeConfig, 60);
 
         }
 
